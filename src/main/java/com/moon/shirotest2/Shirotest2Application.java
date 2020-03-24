bcf8fd465a2be754966ee6553648bbeaf532a69a
moon <1754716900@qq.com>
2020-03-24 22:34:41 +0800

    first

---
+++ src/main/java/com/moon/shirotest2/Shirotest2Application.java
@@ -0,0 +1,13 @@
+package com.moon.shirotest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shirotest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shirotest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/shirotest2/Shirotest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.shirotest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shirotest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
